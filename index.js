f894e0bc9723f7da5742f4b0278a495d6eeb604a
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-04-08 20:50:39 -0300

    Agregando index

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const expressLayouts = require('express-ejs-layouts');
 const router = require('./routes/index');
 const path = require('path');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const passport = require('./config/passport');
 
 //Variables de desarrollo
 require('dotenv').config({path: 'variables.env'});
@@ -21,12 +26,41 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//AGREGAR EXPRESS VALIDATOR
+app.use(expressValidator());
+
 //HABILITAR EJS COMO TEMPLATE ENGINE //ESTO SI O SI TIENE QUE IR ANTES DEL ROUTING SI NO NO EJECUTA expressLayouts
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+
+//UBICACION DE LAS VISTAS
+app.set('views', path.join(__dirname, './views'));
+
+//ARCHIVOS ESTATICOS 
+app.use(express.static('public'));
+
+//HABILITAR COOKIE PARSER
+app.use(cookieParser());
+
+//CREAR LA SESSION 
+app.use(session({
+    secret: process.env.SECRETO,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false
+}))
+
+//INICILIZAR PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+
+//AGREGA FLASH MESSAGES
+app.use(flash());
+
 //MIDDLEWARE (usuario logueado,flash messagues, fecha actual)
 app.use((req,res,next)=>{
+    res.locals.mensajes = req.flash();
     const fecha = new Date();
     res.locals.year= fecha.getFullYear();
     next();
@@ -34,12 +68,6 @@ app.use((req,res,next)=>{
 
 //ROUTING
 app.use('/',router());
-
-//UBICACION DE LAS VISTAS
-app.set('views', path.join(__dirname, './views'));
-
-//ARCHIVOS ESTATICOS 
-app.use(express.static('public'));
 
 //AGREGANDO EL ROUTER
 app.listen(process.env.PORT,() => {
