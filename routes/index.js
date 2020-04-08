a73955d91b6f85cbbf89a4de4bacde82143fe538
Hector Matias Reartes <hmatiasreartes5@hotmail.com>
2020-04-08 20:51:20 -0300

    Metodo post para auntenticar usuario

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,20 @@ const router = express.Router();
 
 const homeController = require('../controllers/homeController');
 const usuariosController = require('../controllers/usuariosController');
+const authController = require('../controllers/authController');
 
 module.exports = function () {
 
     router.get('/', homeController.home);
 
-    //Usuarios
+    //CONFIRMAR Y CREAR CUENTAS
     router.get('/crear-cuenta',usuariosController.formCrearCuenta);
     router.post('/crear-cuenta',usuariosController.crearNuevoUsuario);
+    router.get('/confirmar-cuenta/:correo',usuariosController.confirmarCuenta);
+
+    //iniciar sesion
+    router.get('/iniciar-sesion',usuariosController.formIniciarSesion);
+    router.post('/iniciar-sesion', authController.autenticarUsuario);
 
     return router;
 }
